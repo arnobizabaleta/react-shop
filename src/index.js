9799e625f82387cd0a3db7ace59d4ba03d890e10
Arnobi Zabaleta <arnobizabaleta@gmail.com>
2022-04-01 11:28:47 -0500

    Iniciando con React Router DOM

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("app") );
+//First parametrer: Our App
+//Second parameter is a element when let's do render to our App
