81571bcc2af77bc5ff77de5803b8bc8cf4aadadc
Arnobi Zabaleta <arnobizabaleta@gmail.com>
2022-04-02 11:41:08 -0500

    Origanzando el custom Hook con useEffect para los productItem del productList general de la shop

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -2,24 +2,23 @@ import React,{ useState } from 'react';
 import '../styles/ProductItem.scss';
 import addToCard from "@icons/bt_add_to_cart.svg";
 
-const ProductItem = () => {
-	const [cart,setCart] = useState("Hola");
+const ProductItem = ({product}) => {
+	const [cart,setCart] = useState([]);
 	const handleClick = () => {
-		setCart("Hola mundo");
+		setCart([]);
 	}
 
 	return (
 		<div className="ProductItem">
-			<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
+			<img src={product.images[0]} alt={product.title} />
 			<div className="product-info">
 				<div>
-					<p>$120,00</p>
-					<p>Bike</p>
+					<p>${product.price}</p>
+					<p>{product.title}</p>
 				</div>
 				<figure onClick={handleClick}>
 					<img src={addToCard} alt="" />
 				</figure>
-				{cart}
 			</div>
 		</div>
 	);
